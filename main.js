6faa121ee6048bebb26d6befba374e96e4e48a9c
EricksonDutra <ericksond10@gmail.com>
2024-11-20 16:19:38 -0400

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const video = document.querySelector('video');
-    if (video) {
-        video.play().catch(() => {
-            console.log('Autoplay bloqueado, tentando iniciar manualmente.');
-        });
-    }
+// document.addEventListener('DOMContentLoaded', () => {
+//     const video = document.querySelector('video');
 
-    video.addEventListener('canplay', () => {
-        video.muted = false;
-    })
-});
+//     if (video) {
+//         // Garante que o vídeo está silenciado para o autoplay
+//         video.muted = true;
+//         video.play().catch((error) => {
+//             console.log('Autoplay bloqueado:', error);
+//         });
+
+//         // Remove o mute após interação do usuário
+//         document.addEventListener('canplay', () => {
+//             video.muted = false; // Ativa o som
+//             video.play().catch((error) => {
+//                 console.log('Erro ao reproduzir:', error);
+//             });
+//         });
+//     }
+    
+// });
