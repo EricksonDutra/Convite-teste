8d6775af10327438029deaef00204031406a3b21
EricksonDutra <ericksond10@gmail.com>
2024-11-20 15:13:33 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.querySelector('video');
+    if (video) {
+        video.play().catch(() => {
+            console.log('Autoplay bloqueado, tentando iniciar manualmente.');
+        });
+    }
+});
